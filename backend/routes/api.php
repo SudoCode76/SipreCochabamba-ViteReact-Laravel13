b5130b452cb6819e0454e133f593d3456ae83518
Miguel Code <bo3321319@gmail.com>
2026-05-04 15:02:57 -0400

    fix(backend): se agrego rutas eliminadas por error

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,5 +21,7 @@ Route::prefix('v1')->group(function (): void {
     require __DIR__.'/api/v1/items.php';
     require __DIR__.'/api/v1/projects.php';
     require __DIR__.'/api/v1/inputs.php';
+    require __DIR__.'/api/v1/input_types.php';
+    require __DIR__.'/api/v1/unit_measures.php';
     require __DIR__.'/api/v1/input_requests.php';
 });
