6ed887837e949211c2a74b63e0f92dfcb245a57b
Miguel Code <bo3321319@gmail.com>
2026-05-14 13:38:55 -0400

    feat(backend): Refactorizar el servicio de análisis de precios para soportar diferentes modos de carga de componentes y agregar pruebas para la lógica de precios FNDR.

--- backend/tests/Feature/Item/LegacyUnitPriceAnalysisPdfTest.php
+++ backend/tests/Feature/Item/LegacyUnitPriceAnalysisPdfTest.php
@@ -101,5 +101,7 @@ class LegacyUnitPriceAnalysisPdfTest extends TestCase
         $this->createItemInputRecord(['id_item_insumo' => 1, 'id_item' => 1, 'id_insumo' => 1, 'cantidad' => 2]);
         $this->createItemInputRecord(['id_item_insumo' => 2, 'id_item' => 1, 'id_insumo' => 2, 'cantidad' => 3]);
         $this->createItemInputRecord(['id_item_insumo' => 3, 'id_item' => 1, 'id_insumo' => 3, 'cantidad' => 1]);
+        $this->createInputLog(['id_log' => 1, 'id_insumo' => 2, 'precio' => 100, 'tipo' => 2, 'descripcion' => 'Mano 1', 'fecha' => '2026-05-01']);
+        $this->createInputLog(['id_log' => 2, 'id_insumo' => 3, 'precio' => 200, 'tipo' => 3, 'descripcion' => 'Herramienta 1', 'fecha' => '2026-05-01']);
     }
 }
