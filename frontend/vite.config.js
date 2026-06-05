8623d43b373ab27cc2b49eae6df0c5ec2f8b7757
jampasnando <jampasnando@gmail.com>
2026-06-05 07:24:57 -0400

    fix(vite, jenkins) allowed hosts y parametro -v

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,6 +14,9 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     strictPort: true,
+    allowedHosts: [
+      'sipregamcdev.cochabamba.bo',
+    ],
     watch: {
       usePolling: true,
     },
